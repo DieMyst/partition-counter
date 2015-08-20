00980d6d71717dcaf8dd7644e52546c342641b8e
DieMyst <somedayofmylife@gmail.com>
2015-08-20 04:42:48 +0400

    some beuty

--- src/main/scala/ru/diemyst/Appl.scala
+++ src/main/scala/ru/diemyst/Appl.scala
@@ -14,8 +14,15 @@ object Appl extends App {
     System.exit(1)
   }
 
+  println(
+    """/topic_list for get list of topics
+      |/last_timestamp?topicname= for last timestamp of topic
+      |/last_stats?topicsname= for stats of last timestamp of topic
+      |/last_run_partition_list?topicname= for list of last run partitions
+    """.stripMargin)
+
   if (args.length > 2) {
-    println("to many arguments, bring first")
+    println("to many arguments, get first")
   }
   implicit val system = ActorSystem("test-system", ConfigFactory.load())
   new HttpFrontend(args(0))
